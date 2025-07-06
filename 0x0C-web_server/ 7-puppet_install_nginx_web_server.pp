45e4d53c71c75edfb37497196b8f5d84b50201e4
Anthony <anthonyreagan126@gmail.com>
2025-07-06 09:53:17 +0300

    configuring my server with puppet

--- 0x0C-web_server/ 7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/ 7-puppet_install_nginx_web_server.pp
@@ -1,32 +1,51 @@
 # 7-puppet_install_nginx_web_server.pp
-class nginx_setup {
 
-  # Ensure nginx is installed
-  package { 'nginx':
-    ensure => installed,
-  }
-
-  # Ensure nginx service is running and enabled at boot
-  service { 'nginx':
-    ensure     => running,
-    enable     => true,
-    hasrestart => true,
-  }
-
-  # Create index.html with Hello World!
-  file { '/var/www/html/index.html':
-    ensure  => file,
-    content => "Hello World!\n",
-    require => Package['nginx'],
-  }
-
-  # Ensure custom config is in place
-  file { '/etc/nginx/sites-available/default':
-    ensure  => file,
-    content => template('nginx_setup/default.erb'),
-    require => Package['nginx'],
-    notify  => Service['nginx'],
-  }
+# Update package index
+exec { 'apt-update':
+  command => '/usr/bin/apt-get update',
 }
 
-include nginx_setup
+# Install Nginx
+package { 'nginx':
+  ensure  => installed,
+  require => Exec['apt-update'],
+}
+
+# Ensure Nginx service is running and enabled
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => Package['nginx'],
+}
+
+# Create the default HTML file with Hello World!
+file { '/var/www/html/index.html':
+  content => '<html><body><h1>Hello World!</h1></body></html>',
+  require => Package['nginx'],
+}
+
+# Configure Nginx to include the redirect
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => "
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    root /var/www/html;
+    index index.html index.htm;
+
+    server_name _;
+
+    location / {
+        try_files \$uri \$uri/ =404;
+    }
+
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+}
+  ",
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
