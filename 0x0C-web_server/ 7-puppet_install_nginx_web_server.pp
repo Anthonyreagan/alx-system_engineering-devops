3e4f2281ab7c694118c0b2c13fc2544d9bf20220
Anthony <anthonyreagan126@gmail.com>
2025-07-06 09:56:55 +0300

    configuring my server with puppet

--- 0x0C-web_server/ 7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/ 7-puppet_install_nginx_web_server.pp
@@ -1,39 +1,12 @@
-# 7-puppet_install_nginx_web_server.pp
-
-# Update package index
-exec { 'apt-update':
-  command => '/usr/bin/apt-get update',
-}
-
-# Install Nginx
-package { 'nginx':
-  ensure  => installed,
-  require => Exec['apt-update'],
-}
-
-# Ensure Nginx service is running and enabled
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
-  require => Package['nginx'],
-}
-
-# Create the default HTML file with Hello World!
-file { '/var/www/html/index.html':
-  content => '<html><body><h1>Hello World!</h1></body></html>',
-  require => Package['nginx'],
-}
-
-# Configure Nginx to include the redirect
 file { '/etc/nginx/sites-available/default':
   ensure  => file,
-  content => "
+  content => @(END),
 server {
     listen 80 default_server;
     listen [::]:80 default_server;
 
     root /var/www/html;
-    index index.html index.htm;
+    index index.html;
 
     server_name _;
 
@@ -45,7 +18,7 @@ server {
         return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
     }
 }
-  ",
+  | END
   require => Package['nginx'],
   notify  => Service['nginx'],
 }
